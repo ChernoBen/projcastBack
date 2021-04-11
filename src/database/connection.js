1dd8f0631f7b0d87ff582eff6b5986bd76d07382
benjamim <bneto@computacao.cua.ufmt.br>
2021-04-11 16:32:30 -0300

    db conn

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,8 +2,9 @@ var knex = require('knex')({
     client: 'mysql2',
     connection: {
       host : '127.0.0.1',
+      port:'3306',
       user : 'root',
-      password : '',
+      password : 'Sapiencia123',
       database : 'usertest'
     }
   });
