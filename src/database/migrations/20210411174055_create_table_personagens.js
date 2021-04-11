d501afc014a710d06464ff39bd7fec031f525bdf
benjamim <bneto@computacao.cua.ufmt.br>
2021-04-11 17:56:33 -0300

    primeira migration

--- src/database/migrations/20210411174055_create_table_personagens.js
+++ src/database/migrations/20210411174055_create_table_personagens.js
@@ -1,8 +1,17 @@
-
+//roda sempre que eu pedir pra rodar migrations/cria banco
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('personagens',function(table){
+    table.increments('id')
+    table.text('autor').notNullable()
+    table.text('titulo').notNullable()
+    table.text('conteudo').notNullable()
+    table.timestamp('date').defaultTo(knex.fn.now())
+    table.timestamp('atualizacao').defaultTo(knex.fn.now())
+
+  })
 };
 
+//roolback função de execução de backup
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('personagens')
 };
