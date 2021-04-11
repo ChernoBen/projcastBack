95b25c5ab623882271dd855e38e6b22b568457aa
benjamim <bneto@computacao.cua.ufmt.br>
2021-04-11 20:09:28 -0300

    token migration success

--- src/database/migrations/20210411194903_create_table_password_token.js
+++ src/database/migrations/20210411194903_create_table_password_token.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('token',function(table){
         table.integer('user').unsigned().references("id").inTable("users")
         table.integer("used").defaultTo(0)
-        table.string("token").defaultTo(knex.fn.now())
+        table.string("token").defaultTo(Date.now())
     })
   
 };
